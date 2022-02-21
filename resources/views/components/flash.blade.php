eead26ad96dc1a2512b22eb22d65ce25b74f153f
Shiba Takahiro <t_shiba@miraino-katachi.co.jp>
2022-02-21 14:30:15 +0900

    フラッシュメッセージのタイムアウトをなくした

--- resources/views/components/flash.blade.php
+++ resources/views/components/flash.blade.php
@@ -1,6 +1,6 @@
 @if (session()->has('success'))
     <div x-data="{ show: true }"
-         x-init="setTimeout(() => show = false, 4000)"
+         {{-- x-init="setTimeout(() => show = false, 4000)" --}}
          x-show="show"
          class="fixed shadow bg-white text-cyan-500 py-2 px-8 rounded-full bottom-3 right-3 text-sm"
     >
